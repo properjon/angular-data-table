9507d45ea1c322c6b624cb982b5ad892e6b14962
Jonathan Shaffer <jonshaffer@Jonathans-iMac.lan>
2016-12-11 22:06:13 -0500

    adding more karma options for saucelabs in travis

--- test/karma.conf.babel.js
+++ test/karma.conf.babel.js
@@ -81,13 +81,27 @@ export default (config) => {
   config.set(karmaBaseConfig);
 
   if (process.env.TRAVIS) {
-    config.sauceLabs = {
-      testName: 'angular-data-table unit tests'
+    if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+      console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.')
+      process.exit(1)
+    }
+
+    config.sauceLabs: {
+      testName: 'angular-data-table unit tests',
+      recordScreenshots: false,
+      connectOptions: {
+        port: 5757,
+        logfile: 'sauce_connect.log'
+      },
+      public: 'public',
+      tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER,
+      startConnect: false
     };
-    config.startConnect = false;
-    config.tunnelIdentifier = process.env.TRAVIS_JOB_NUMBER;
+
     config.customLaunchers = customLaunchers;
+
     config.browsers = Object.keys(customLaunchers);
+
     config.reporters = ['progress', 'dots', 'coverage', 'saucelabs'];
   }
 };
