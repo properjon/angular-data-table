3adf2d043bdf11e1bc7df55c5b1539085ccdd056
Austin <austin.laird07@gmail.com>
2016-12-28 09:06:20 -0500

    Initial version of the e2e tests for multiple column sorting

--- src/components/DataTableDirective.e2e.js
+++ src/components/DataTableDirective.e2e.js
@@ -4,4 +4,29 @@ describe('DataTableDirective', function () {
 
     expect(element.all(by.css('dt-body')).count()).toBe(1);
   });
+
+  describe('sorting', function () {
+      it('be able to sort by multiple columns', function() {
+          browser.get('http://localhost:9000/demos/sort.html');
+
+          var colHeaderList = by.repeater('column in header.columns[\'center\'] track by column.$id'),
+               colHeader1 = element(colHeaderList.row(0)),
+               colHeader1Icon = colHeader1.element(by.css('.sort-btn')),
+               colHeader2 = element(colHeaderList.row(1)),
+               colHeader2Icon = colHeader2.element(by.css('.sort-btn')),
+               row1Column1 = element.all(by.css('.dt-cell')).first();
+
+          colHeader1.click();
+
+          expect(hasClass(colHeader1Icon, 'icon-up')).toBe(true);
+          expect(hasClass(colHeader2Icon, 'icon-down')).toBe(true);
+          expect(row1Column1.getText()).toBe('Valarie Atkinson');
+      });
+  });
+});
+
+var hasClass = function (element, cls) {
+    return element.getAttribute('class').then(function (classes) {
+        return classes.split(' ').indexOf(cls) !== -1;
+    });
 });
