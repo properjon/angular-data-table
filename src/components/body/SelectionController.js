c0bcb98f4274e2d53985da26ae4f4b3a0a12b3b0
Jon Shaffer <jon@hyperfluidsolutions.com>
2017-01-04 11:49:53 -0500

    onchanges, linting updates

--- src/components/body/SelectionController.js
+++ src/components/body/SelectionController.js
@@ -1,9 +1,9 @@
 import { KEYS } from '../../utils/keys';
 
-export class SelectionController {
+export default class SelectionController {
 
-  /*@ngInject*/
-  constructor($scope){
+  /* @ngInject*/
+  constructor($scope) {
     this.body = $scope.body;
     this.options = $scope.body.options;
     this.selected = $scope.body.selected;
@@ -15,22 +15,22 @@ export class SelectionController {
    * @param  {index}
    * @param  {row}
    */
-  keyDown(ev, index, row){
-    if(KEYS[ev.keyCode]){
+  keyDown(ev, index, row) {
+    if (KEYS[ev.keyCode]) {
       ev.preventDefault();
     }
 
     if (ev.keyCode === KEYS.DOWN) {
-      var next = ev.target.nextElementSibling;
-      if(next){
+      const next = ev.target.nextElementSibling;
+      if (next) {
         next.focus();
       }
     } else if (ev.keyCode === KEYS.UP) {
-      var prev = ev.target.previousElementSibling;
-      if(prev){
+      const prev = ev.target.previousElementSibling;
+      if (prev) {
         prev.focus();
       }
-    } else if(ev.keyCode === KEYS.RETURN){
+    } else if (ev.keyCode === KEYS.RETURN) {
       this.selectRow(index, row);
     }
   }
@@ -41,28 +41,28 @@ export class SelectionController {
    * @param  {int} index
    * @param  {object} row
    */
-  rowClicked(event, index, row){
-    if(!this.options.checkboxSelection){
+  rowClicked(event, index, row) {
+    if (!this.options.checkboxSelection) {
       // event.preventDefault();
       this.selectRow(event, index, row);
     }
 
-    this.body.onRowClick({ row: row });
+    this.body.onRowClick({ row });
   }
-  
+
   /**
    * Handler for the row double click event
    * @param  {object} event
    * @param  {int} index
    * @param  {object} row
    */
-  rowDblClicked(event, index, row){
-    if(!this.options.checkboxSelection){
+  rowDblClicked(event, index, row) {
+    if (!this.options.checkboxSelection) {
       event.preventDefault();
       this.selectRow(event, index, row);
     }
 
-    this.body.onRowDblClick({ row: row });
+    this.body.onRowDblClick({ row });
   }
 
   /**
@@ -70,7 +70,7 @@ export class SelectionController {
    * @param  {index}
    * @param  {row}
    */
-  onCheckboxChange(event, index, row){
+  onCheckboxChange(event, index, row) {
     this.selectRow(event, index, row);
   }
 
@@ -79,30 +79,30 @@ export class SelectionController {
    * @param  {index}
    * @param  {row}
    */
-  selectRow(event, index, row){
-    if(this.options.selectable){
-      if(this.options.multiSelect){
-        var isCtrlKeyDown = event.ctrlKey || event.metaKey,
-            isShiftKeyDown = event.shiftKey;
+  selectRow(event, index, row) {
+    if (this.options.selectable) {
+      if (this.options.multiSelect) {
+        const isCtrlKeyDown = event.ctrlKey || event.metaKey;
+        const isShiftKeyDown = event.shiftKey;
 
-        if(isShiftKeyDown){
+        if (isShiftKeyDown) {
           this.selectRowsBetween(index, row);
         } else {
-          var idx = this.selected.indexOf(row);
-          if(idx > -1){
+          const idx = this.selected.indexOf(row);
+          if (idx > -1) {
             this.selected.splice(idx, 1);
           } else {
-            if(this.options.multiSelectOnShift && this.selected.length === 1) {
+            if (this.options.multiSelectOnShift && this.selected.length === 1) {
               this.selected.splice(0, 1);
             }
             this.selected.push(row);
-            this.body.onSelect({ rows: [ row ] });
+            this.body.onSelect({ rows: [row] });
           }
         }
         this.prevIndex = index;
       } else {
         this.selected = row;
-        this.body.onSelect({ rows: [ row ] });
+        this.body.onSelect({ rows: [row] });
       }
     }
   }
@@ -111,43 +111,45 @@ export class SelectionController {
    * Selects the rows between a index.  Used for shift click selection.
    * @param  {index}
    */
-  selectRowsBetween(index){
-    var reverse = index < this.prevIndex,
-        selecteds = [];
+  selectRowsBetween(index) {
+    const reverse = index < this.prevIndex;
+    const selecteds = [];
 
-    for(var i=0, len=this.body.rows.length; i < len; i++) {
-      var row = this.body.rows[i],
-          greater = i >= this.prevIndex && i <= index,
-          lesser = i <= this.prevIndex && i >= index;
+    for (let i = 0, len = this.body.rows.length; i < len; i += 1) {
+      const row = this.body.rows[i];
+      const greater = i >= this.prevIndex && i <= index;
+      const lesser = i <= this.prevIndex && i >= index;
 
-      var range = {};
-      if ( reverse ) {
+      let range = {};
+
+      if (reverse) {
         range = {
           start: index,
-          end: ( this.prevIndex - index )
-        }
+          end: (this.prevIndex - index),
+        };
       } else {
         range = {
           start: this.prevIndex,
-          end: index + 1
-        }
+          end: index + 1,
+        };
       }
 
-      if((reverse && lesser) || (!reverse && greater)){
-        var idx = this.selected.indexOf(row);
+      if ((reverse && lesser) || (!reverse && greater)) {
+        const idx = this.selected.indexOf(row);
+
         // if reverse shift selection (unselect) and the
         // row is already selected, remove it from selected
-        if ( reverse && idx > -1 ) {
-          this.selected.splice(idx, 1);
-          continue;
-        }
-        // if in the positive range to be added to `selected`, and
-        // not already in the selected array, add it
-        if( i >= range.start && i < range.end ){
-          if ( idx === -1 ) {
-            this.selected.push(row);
-            selecteds.push(row);
+        if (!reverse || idx <= -1) {
+          // if in the positive range to be added to `selected`, and
+          // not already in the selected array, add it
+          if (i >= range.start && i < range.end) {
+            if (idx === -1) {
+              this.selected.push(row);
+              selecteds.push(row);
+            }
           }
+        } else {
+          this.selected.splice(idx, 1);
         }
       }
     }
