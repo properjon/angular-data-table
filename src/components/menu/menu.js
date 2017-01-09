34989fb7ac16d6add3c0419b4dd8cb18011a1dd3
Jon Shaffer <jon@hyperfluidsolutions.com>
2017-01-09 17:02:35 -0500

    missing popover, menu angular imports

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,3 +1,5 @@
+import angular from 'angular';
+
 import { MenuController } from './MenuController';
 import { MenuDirective } from './MenuDirective';
 import dropdown from './dropdown/dropdown';
--- src/components/popover/popover.js
+++ src/components/popover/popover.js
@@ -1,3 +1,5 @@
+import angular from 'angular';
+
 import { PopoverDirective } from './PopoverDirective';
 import { PopoverRegistry } from './PopoverRegistry';
 import { PositionHelper } from './PositionHelper';
