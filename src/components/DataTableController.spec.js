18fdcf6782f69f3441d32cc34a82da92ae9dd73d
Jon Shaffer <jon@hyperfluidsolutions.com>
2017-01-09 17:05:17 -0500

    starting datatablecontroller unit tests

--- src/components/DataTableController.spec.js
+++ src/components/DataTableController.spec.js
@@ -3,11 +3,30 @@ import { DataTableController } from './DataTableController';
 describe('DataTableController', function () {
   let $controller = null;
 
-  beforeEach(
-    inject((_$controller_) => {
-      $controller = _$controller_;
-    })
-  );
+  beforeEach(inject((_$rootScope_, _$controller_) => {
+    $controller = _$controller_;
+    $rootScope = _$rootScope_;
+  }));
+
+  beforeEach(() => {
+    setController = (bindings) => {
+      bindings.options = Object.assign({}, TableDefaults, bindings.options);
+
+      scope = $rootScope.$new();
+
+      scope.body = {
+        options: bindings.options,
+        rows: bindings.rows
+      };
+
+      ctrl = $controller('DataTableController',
+        {
+          $scope: scope
+        },
+        bindings
+      );
+    };
+  });
 
   it('should export a function', () => {
     expect(DataTableController).toEqual(jasmine.any(Function));
