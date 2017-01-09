4ca03b328ef139f61bf72a8207604d52f3a151b0
Jon Shaffer <jon@hyperfluidsolutions.com>
2017-01-09 17:23:24 -0500

    fixed icons, material styles. rebuild. fixed popover registry to be factory

--- src/components/popover/PopoverRegistry.js
+++ src/components/popover/PopoverRegistry.js
@@ -5,28 +5,31 @@
 
 export default function PopoverRegistry($animate) {
   const popovers = {};
-  this.add = (id, object) => {
-    popovers[id] = object;
+
+  return {
+    add(id, object) {
+      popovers[id] = object;
+    },
+
+    find(id) {
+      return popovers[id];
+    },
+
+    remove(id) {
+      delete popovers[id];
+    },
+
+    removeGroup(group, currentId) {
+      return angular.forEach(popovers, (popoverOb, id) => {
+        if (id === currentId) return;
+
+        if (popoverOb.group && popoverOb.group === group) {
+          $animate.removeClass(popoverOb.popover, 'sw-popover-animate').then(() => {
+            popoverOb.popover.remove();
+            delete popovers[id];
+          });
+        }
+      });
+    },
   };
-
-  this.find = id => (
-    popovers[id]
-  );
-
-  this.remove = (id) => {
-    delete popovers[id];
-  };
-
-  this.removeGroup = (group, currentId) => (
-    angular.forEach(popovers, (popoverOb, id) => {
-      if (id === currentId) return;
-
-      if (popoverOb.group && popoverOb.group === group) {
-        $animate.removeClass(popoverOb.popover, 'sw-popover-animate').then(() => {
-          popoverOb.popover.remove();
-          delete popovers[id];
-        });
-      }
-    })
-  );
 }
