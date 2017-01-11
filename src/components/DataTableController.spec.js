f9a4ad62f85f289bc16c8ae6642cdcab59541ef4
Austin <austin.laird07@gmail.com>
2017-01-11 13:23:08 -0500

    Added unit test for intializing column defaults

--- src/components/DataTableController.spec.js
+++ src/components/DataTableController.spec.js
@@ -35,7 +35,7 @@ describe('DataTableController', () => {
         { name: 'Maude', age: 48 }
       ];
 
-      setController({ 
+      setController({
         options: options,
         rows: rows
       });
@@ -64,6 +64,87 @@ describe('DataTableController', () => {
       sortedAscending = ctrl.rows[0].name < ctrl.rows[1].name;
 
       expect(sortedAscending).toBe(false);
-    });    
+    });
+  });
+
+  describe('initializations', () => {
+      describe('setting column defaults', function () {
+          beforeEach(() => {
+              let options = {
+                columns: [
+                  { prop: 'name', sort: 'asc' },
+                  { prop: 'age'}
+                ]
+              };
+              let rows = [
+                { name: 'Walter', age: 49 },
+                { name: 'Dude', age: 45 },
+                { name: 'Donnie', age: 46 },
+                { name: 'Maude', age: 48 }
+              ];
+
+              setController({
+                options: options,
+                rows: rows
+              });
+
+              ctrl.transposeColumnDefaults();
+          });
+
+          it('should set the column defaults', () => {
+             var col = ctrl.options.columns[0];
+             delete col.$id;
+             expect(col).toEqual({
+                 prop: 'name',
+                 sort: 'asc',
+                 frozenLeft: false,
+                 frozenRight: false,
+                 className: undefined,
+                 headerClassName: undefined,
+                 flexGrow: 0,
+                 minWidth: 100,
+                 maxWidth: undefined,
+                 width: 150,
+                 resizable: true,
+                 comparator: undefined,
+                 sortable: true,
+                 sortBy: undefined,
+                 headerRenderer: undefined,
+                 cellRenderer: undefined,
+                 cellDataGetter: undefined,
+                 group: false,
+                 isTreeColumn: false,
+                 isCheckboxColumn: false,
+                 headerCheckbox: false,
+                 canAutoResize: true
+             });
+             col = ctrl.options.columns[1];
+             delete col.$id;
+             expect(col).toEqual({
+                 prop: 'age',
+                 sort: undefined,
+                 frozenLeft: false,
+                 frozenRight: false,
+                 className: undefined,
+                 headerClassName: undefined,
+                 flexGrow: 0,
+                 minWidth: 100,
+                 maxWidth: undefined,
+                 width: 150,
+                 resizable: true,
+                 comparator: undefined,
+                 sortable: true,
+                 sortBy: undefined,
+                 headerRenderer: undefined,
+                 cellRenderer: undefined,
+                 cellDataGetter: undefined,
+                 group: false,
+                 isTreeColumn: false,
+                 isCheckboxColumn: false,
+                 headerCheckbox: false,
+                 canAutoResize: true
+             });
+          });
+      });
   });
 });
