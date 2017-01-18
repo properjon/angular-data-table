e2ebf7a566fbda3fd9cd8fbff7c53946544c8a30
Ben Sgroi <bensgroi@outlook.com>
2017-01-18 15:42:46 -0500

    Fixes/improvements to existing protractor tests

--- test/protractor/Paging.e2e.js
+++ test/protractor/Paging.e2e.js
@@ -1,39 +1,42 @@
-describe('When Paging:', function () {
-  browser.get('/demos/paging.html');
+const PagingPage = require('./paging-page');
+
+describe('When Paging:', () => {
+  let page;
+  
+  beforeEach(() => {
+    page = new PagingPage();
+    page.get();
+  });
 
   it('should have active class on first pager', () => {
-    element.all(by.css('ul.pager li')).then(function(items) {
+    element.all(by.css('ul.pager li')).then(items => {
       expect(items.length).toBe(9);
       expect(items[2].getAttribute('class')).toContain('active');
     });
   });
 
   it('should display 12 rows', () => {
-    element.all(by.css('.dt-row')).then(function(items) {
-      expect(items.length).toBe(12);
-    });
+    page.getCurrentPageRows().then(rows => {
+      expect(rows.length).toBe(12);
+    }); 
   });
 
   it('should advance to the second page', () => {
-    let nextButton = $$('.icon-right');
-
     element.all(by.css('.dt-cell')).first().getText().then(firstVal => {
-      nextButton.click();
+      page.goToNextPage();
 
       $$('.dt-cell').first().getText().then(secondVal => {
         expect(firstVal).not.toBe(secondVal);
-      }); 
+      });
     });
   });
 
   // 100 % 12 = 4
   it('should have only 4 rows on the last page', () => {
-    let lastButton = $$('.icon-skip');
+    page.goToLastPage();
 
-    lastButton.click();
-
-    element.all(by.css('.dt-row')).then((items) => {
-      expect(items.length).toBe(4);
+    page.getCurrentPageRows().then(rows => {
+      expect(rows.length).toBe(4);
     });
   });
 });
