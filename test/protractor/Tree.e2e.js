a1cb90056956a54e1f542c45575b08ba8ec2015f
Ben Sgroi <bensgroi@outlook.com>
2017-01-19 08:49:57 -0500

    Began tree protractor tests

---
+++ test/protractor/Tree.e2e.js
@@ -0,0 +1,12 @@
+const TreePage = require('./tree-page');
+
+describe('On Tree Page: ', () => {
+    let page;
+
+    beforeEach(() => {
+        page = new TreePage();
+        page.get();
+    });
+
+    // TODO - tests here
+});
---
+++ test/protractor/tree-page.js
@@ -0,0 +1,7 @@
+class TreePage {
+    get() {
+        browser.get('/demos/tree.html');
+    }
+}
+
+module.exports = TreePage;
