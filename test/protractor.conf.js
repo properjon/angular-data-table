0de7bd702d66b8f3f7d765e85d48c874a5986fdd
Jon Shaffer <jon@hyperfluidsolutions.com>
2017-01-25 12:35:45 -0500

    adding windows 10 ie 11 to protractor multicompatibilities

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -12,11 +12,21 @@ if (process.env.TRAVIS) {
     {
       username: process.env.SAUCE_USERNAME,
       accessKey: process.env.SAUCE_ACCESS_KEY,
-      name: 'angular-data-table e2e tests: chrome',
+      name: 'e2e tests: chrome',
       browserName: 'chrome',
       'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
       build: process.env.TRAVIS_BUILD_NUMBER,
     },
+    {
+      username: process.env.SAUCE_USERNAME,
+      accessKey: process.env.SAUCE_ACCESS_KEY,
+      name: 'e2e tests: ie11',
+      browserName: 'internet explorer',
+      platform: 'Windows 10',
+      version: '11.103',
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+      build: process.env.TRAVIS_BUILD_NUMBER,
+    }
   ];
 }
 
