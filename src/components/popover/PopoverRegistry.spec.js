7817379f450a6c7d32edaf455673ddd16c503dda
Nathan Smyth <nathansmyth@users.noreply.github.com>
2017-02-16 16:32:58 -0500

    Popover timeout handlers (#77)
    * Add timeout handlers for mousemove and mouseleave
    
    Refactor .display() by extracting 3 functions
    
    * Fixes for codeclimate
    
    * Add scope on destroy
    
    Add comments to new functions
    
    Refactor multiple if's to switch statement
    
    * Refactor switch to if else's
    
    Other codeclimate changes
    
    * Remove group from demo and group'ing code
    
    Format demo page
    
    * Update popover id to row index from timestamp
    
    Remove .removeGroup() from PopoverRegistry
    
    * Fixes linting issues
    
    * Add first protractor tests
    
    * Add failing test for #57
    
    * Remove dom cleanup code - was throwing errors
    
    Refactor popover dom creation
    
    * Remove animate injection for code climate
    
    * Update syntax to avoid protractor warning message
    
    * Add failing protractor tests for popovers closing
    
    * Update test to move mouse far away from popup
    
    * Add specs for PopoverRegistry
    
    * Add popoverId constant
    
    * Update skipped internal constant spec to run
    
    * Update vertical center to middle
    
    Add specs for PositionHelper, have coverage now need to pass
    
    * Add proper mocks from @jonshaffer
    
    Add middle to constants
    
    Update specs to pass
    
    * Update default alignment and add height for IE11
    
    * Update protractor tests to pass locally
    
    * Add spec for PopoverDirective
    
    * Add basic directive tests
    
    Update order of dependency injections

--- src/components/popover/PopoverRegistry.spec.js
+++ src/components/popover/PopoverRegistry.spec.js
@@ -1,5 +1,30 @@
-// import PopoverRegistry from './PopoverRegistry';
+import PopoverRegistry from './PopoverRegistry';
 
 describe('PopoverRegistry', () => {
-  //
+  const registry = new PopoverRegistry();
+  const popoverId = 0;
+  const popoverObject = { element: 'JQLite', popover: 'JQLite' };
+
+  it('adds a popover object to the registry', () => {
+    const popover = registry.add(popoverId, popoverObject);
+    expect(popover).toEqual(popoverObject);
+  });
+
+  it('denies me the internal popovers object', () => {
+    expect(registry.popovers).not.toEqual(jasmine.any(Object));
+  });
+
+  describe('popover already exists', () => {
+    beforeEach(() => {
+      registry.add(popoverId, popoverObject);
+    });
+
+    it('returns a popover by id', () => {
+      expect(registry.find(popoverId)).toEqual(jasmine.any(Object));
+    });
+
+    it('removes a popover by id', () => {
+      expect(registry.remove(popoverId)).toBeUndefined();
+    });
+  });
 });
--- src/components/popover/PositionHelper.spec.js
+++ src/components/popover/PositionHelper.spec.js
@@ -1,5 +1,139 @@
-// import PositionHelper from './PositionHelper';
+import PositionHelper from './PositionHelper';
+import POPOVER from './Popover.constants';
 
 describe('PositionHelper', () => {
-  //
+  const elementBox = {
+    bottom: 138,
+    height: 25,
+    left: 595.9375,
+    right: 6147.984,
+    top: 113,
+    width: 5553.04538,
+  };
+  const popoverBox = {
+    bottom: 275.5,
+    height: 300,
+    left: 342.8125,
+    right: 592.8125,
+    top: -24.5,
+    width: 250,
+  };
+  const caretBox = {
+    bottom: 508.5,
+    height: 11,
+    left: 480.328125,
+    right: 486.328125,
+    top: 497.5,
+    width: 6,
+  };
+
+  let positionHelper = null,
+    logMock = null,
+    getPositionHelper = null;
+
+  beforeEach(() => {
+    getPositionHelper = () => {
+      logMock = {
+        warn: jasmine.createSpy('warn')
+      };
+
+      positionHelper = new PositionHelper(logMock);
+    };
+  });
+
+  describe('when calculating popover alignment', () => {
+    beforeEach(() => {
+      getPositionHelper();
+    });
+
+    it('uses two functions to calculate popover alignment', () => {
+      expect(positionHelper.calculateHorizontalAlignment).toEqual(jasmine.any(Function));
+      expect(positionHelper.calculateVerticalAlignment).toEqual(jasmine.any(Function));
+    });
+
+    it('uses two functions to calculate caret alignment', () => {
+      expect(positionHelper.calculateHorizontalCaret).toEqual(jasmine.any(Function));
+      expect(positionHelper.calculateVerticalCaret).toEqual(jasmine.any(Function));
+    });
+
+    it('calculate horizontal alignment with invalid value', () => {
+      positionHelper.calculateHorizontalAlignment(elementBox, popoverBox, '');
+      expect(logMock.warn).toHaveBeenCalled();
+    });
+
+    it('calculate horizontal alignment with center value', () => {
+      const calculated = positionHelper.calculateHorizontalAlignment(elementBox, popoverBox, POPOVER.CENTER);
+      expect(calculated).toEqual(3247.46019);
+    });
+
+    it('calculate horizontal alignment with left value', () => {
+      const calculated = positionHelper.calculateHorizontalAlignment(elementBox, popoverBox, POPOVER.LEFT);
+      expect(calculated).toEqual(595.9375);
+    });
+
+    it('calculate horizontal alignment with right value', () => {
+      const calculated = positionHelper.calculateHorizontalAlignment(elementBox, popoverBox, POPOVER.RIGHT);
+      expect(calculated).toEqual(5898.98288);
+    });
+
+    it('calculate vertical alignment with invalid value', () => {
+      positionHelper.calculateVerticalAlignment(elementBox, popoverBox, '');
+      expect(logMock.warn).toHaveBeenCalled();
+    });
+
+    it('calculate vertical alignment with bottom value', () => {
+      const calculated = positionHelper.calculateVerticalAlignment(elementBox, popoverBox, POPOVER.BOTTOM);
+      expect(calculated).toEqual(-162);
+    });
+
+    it('calculate vertical alignment with middle value', () => {
+      const calculated = positionHelper.calculateVerticalAlignment(elementBox, popoverBox, POPOVER.MIDDLE);
+      expect(calculated).toEqual(-24.5);
+    });
+
+    it('calculate vertical alignment with top value', () => {
+      const calculated = positionHelper.calculateVerticalAlignment(elementBox, popoverBox, POPOVER.TOP);
+      expect(calculated).toEqual(113);
+    });
+
+    it('calculate horizontal caret alignment with invalid value', () => {
+      positionHelper.calculateHorizontalCaret(elementBox, popoverBox, caretBox, '');
+      expect(logMock.warn).toHaveBeenCalled();
+    });
+
+    it('calculate horizontal caret alignment with center value', () => {
+      const calculated = positionHelper.calculateHorizontalCaret(elementBox, popoverBox, caretBox, POPOVER.CENTER);
+      expect(calculated).toEqual(118.5);
+    });
+
+    it('calculate horizontal caret alignment with left value', () => {
+      const calculated = positionHelper.calculateHorizontalCaret(elementBox, popoverBox, caretBox, POPOVER.LEFT);
+      expect(calculated).toEqual(2770.02269);
+    });
+
+    it('calculate horizontal caret alignment with right value', () => {
+      const calculated = positionHelper.calculateHorizontalCaret(elementBox, popoverBox, caretBox, POPOVER.RIGHT);
+      expect(calculated).toEqual(-2533.02269);
+    });
+
+    it('calculate vertical caret alignment with invalid value', () => {
+      positionHelper.calculateVerticalCaret(elementBox, popoverBox, caretBox, '');
+      expect(logMock.warn).toHaveBeenCalled();
+    });
+
+    it('calculate vertical caret alignment with bottom value', () => {
+      const calculated = positionHelper.calculateVerticalCaret(elementBox, popoverBox, caretBox, POPOVER.BOTTOM);
+      expect(calculated).toEqual(281);
+    });
+
+    it('calculate vertical caret alignment with middle value', () => {
+      const calculated = positionHelper.calculateVerticalCaret(elementBox, popoverBox, caretBox, POPOVER.MIDDLE);
+      expect(calculated).toEqual(143.5);
+    });
+
+    it('calculate vertical caret alignment with top value', () => {
+      const calculated = positionHelper.calculateVerticalCaret(elementBox, popoverBox, caretBox, POPOVER.TOP);
+      expect(calculated).toEqual(6);
+    });
+  });
 });
